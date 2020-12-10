d226e8f93797a9e62588956d54a865ca06f5e4d0
Thomas Vanbesien <tvanbesi@student.42.fr>
2020-12-10 09:08:59 +0100

    exit builtin

--- builtin.c
+++ builtin.c
@@ -6,7 +6,7 @@
 /*   By: tvanbesi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 16:28:11 by tvanbesi          #+#    #+#             */
-/*   Updated: 2020/12/09 15:49:12 by tvanbesi         ###   ########.fr       */
+/*   Updated: 2020/12/10 09:07:59 by tvanbesi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void
 	ft_builtin(char	*cmd, t_list *argv, t_shell *shell)
 {
+	//TODO: return values
 	if (!ft_strncmp(cmd, "pwd", 4))
 		ft_pwd(argv, shell);
 	else if (!ft_strncmp(cmd, "cd", 3))
@@ -25,6 +26,8 @@ void
 		ft_export(argv, shell);
 	else if (!ft_strncmp(cmd, "unset", 6))
 		ft_unset(argv, shell);
+	else if (!ft_strncmp(cmd, "exit", 5))
+		exit(0);
 	else
 		printf("command not found\n");
 }
